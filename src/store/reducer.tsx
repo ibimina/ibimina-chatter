e411e630ceb274f26e06e09572a3a76cfa52a132
Ibimina hart <ibimina.c.hart@gmail.com>
2023-05-18 18:55:45 +0100

    docs: define user property

--- src/store/reducer.tsx
+++ src/store/reducer.tsx
@@ -2,69 +2,78 @@ import { createContext, useEffect, useReducer } from 'react';
 import { AUTH_STATE_CHANGED, LOG_IN, SIGN_IN, SIGN_OUT } from './action';
 import { firebaseAuth } from '@/firebase/config';
 import { onAuthStateChanged } from 'firebase/auth';
-import { UserProps } from '@/types/user';
 
 export const InitialState = {
-	user: {} as UserProps,
-	authState: false,
+    user: {
+        uid: '',
+        email: '',
+        username: '',
+        photoURL: '',
+    },
+    authState: false,
 };
 
 export const authReducer = (
-	state = InitialState,
-	action: { type: string; payload: any }
+    state = InitialState,
+    action: { type: string; payload: any }
 ) => {
-	if (action.type === SIGN_IN) {
-		return {
-			...state,
-			user: action.payload,
-		};
-	}
-	if (action.type === LOG_IN) {
-		return {
-			...state,
-			user: action.payload,
-		};
-	}
-	if (action.type === SIGN_OUT) {
-		return {
-			...state,
-			user: null,
-		};
-	}
-	if (action.type === AUTH_STATE_CHANGED) {
-		return {
-			...state,
-			authState: true,
-			user: action.payload,
-		};
-	}
-	return state;
+    if (action.type === SIGN_IN) {
+        return {
+            ...state,
+            user: action.payload,
+        };
+    }
+    if (action.type === LOG_IN) {
+        return {
+            ...state,
+            user: action.payload,
+        };
+    }
+    if (action.type === SIGN_OUT) {
+        return {
+            ...state,
+            user: null,
+        };
+    }
+    if (action.type === AUTH_STATE_CHANGED) {
+        return {
+            ...state,
+            authState: true,
+            user: action.payload,
+        };
+    }
+    return state;
 };
 export const AuthContext = createContext({
-	state: InitialState,
-	dispatch: (action: { type: string; payload: any }) => {},
+    state: InitialState,
+    dispatch: (action: { type: string; payload: any }) => { },
 });
 export const AuthContextProvider = ({
-	children,
+    children,
 }: {
-	children: React.ReactNode;
+    children: React.ReactNode;
 }) => {
-	const [state, dispatch] = useReducer(authReducer, InitialState);
-	useEffect(() => {
-		const unsub = onAuthStateChanged(firebaseAuth, (user) => {
-			if (user) {
-				dispatch({
-					type: AUTH_STATE_CHANGED,
-					payload: user,
-				});
-			}
-		});
-		unsub();
-	}, []);
+    const [state, dispatch] = useReducer(authReducer, InitialState);
+    useEffect(() => {
+        const unsub = onAuthStateChanged(firebaseAuth, (user) => {
+            if (user) {
+                dispatch({
+                    type: AUTH_STATE_CHANGED,
+                    payload: user,
+                });
+            }else{
+                dispatch({
+                    type: AUTH_STATE_CHANGED,
+                    payload: null,
+                });
+            }
+        });
+        unsub();
+    }, []);
 
-	return (
-		<AuthContext.Provider value={{ state, dispatch }}>
-			{children}
-		</AuthContext.Provider>
-	);
+    return (
+        <AuthContext.Provider value={{ state, dispatch }}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
