56d9a667562a103040d2593a531265547c2c95ba
Ibimina hart <ibimina.c.hart@gmail.com>
2023-05-18 16:04:22 +0100

    docs: sign up hook

---
+++ src/hooks/useSignUp.ts
@@ -0,0 +1,36 @@
+import { firebaseAuth } from "@/firebase/config";
+import {  signIn } from "@/store/action";
+import { useAuthContext } from "@/store/store";
+import { SignUpProps } from "@/types/signup";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useState } from "react";
+
+
+export default function useSignUp() {
+    const { dispatch } = useAuthContext();
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const createUser = async ({ username, email, password }: SignUpProps) => {
+        try {
+            setIsLoading(true);
+            const userCredential = await createUserWithEmailAndPassword(firebaseAuth, email, password)
+            await updateProfile(userCredential.user, {
+                displayName: username,
+            })
+            // Signed in 
+            const userRef = userCredential.user;
+            dispatch(signIn(userRef));
+            setIsLoading(false);
+        } catch (error: any) {
+            setError(error.message);
+            setIsLoading(false);
+            console.log(error);
+        }
+    };
+    return { createUser, error, isLoading };
+}
+
+
+
+
