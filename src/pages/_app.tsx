86906e5efec54e6c9aec3db2e883acdfa647d0a1
Ibimina hart <ibimina.c.hart@gmail.com>
2023-05-18 16:02:43 +0100

    docs: wrap component with context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
+import { AuthContextProvider } from "@/store/reducer";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+  return (
+    <AuthContextProvider>
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  )
 }
