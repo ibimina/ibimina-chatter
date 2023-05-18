73a4a44845aed7e32ce809845bf4a7d168bfca7b
Ibimina hart <ibimina.c.hart@gmail.com>
2023-05-18 18:58:52 +0100

    docs: route guard

--- src/pages/chatter.tsx
+++ src/pages/chatter.tsx
@@ -1,4 +1,15 @@
+import { useAuthContext } from "@/store/store";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 function Chatter() {
+	const { state } = useAuthContext();
+	const router = useRouter();
+	useEffect(() => {
+		if (state?.user?.uid?.length! <= 0) {
+			router.push('/')
+		}
+	},[router, state.user]);
 	return (
 		<>
 			<h1>hello</h1>
