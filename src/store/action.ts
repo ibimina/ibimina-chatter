4bbf5c3290b73572480609304a1b9cd283626030
Ibimina hart <ibimina.c.hart@gmail.com>
2023-05-18 16:00:45 +0100

    docs: dispatch action type

---
+++ src/store/action.ts
@@ -0,0 +1,25 @@
+import { UserProps } from '@/types/user';
+
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_OUT = 'SIGN_OUT';
+export const LOG_IN = 'LOG_IN';
+export const AUTH_STATE_CHANGED = 'AUTH_STATE_CHANGED';
+
+export const signIn = (user: UserProps) => ({
+    type: SIGN_IN,
+    payload: user,
+});
+
+export const signOut = () => ({
+    type: SIGN_OUT,
+});
+
+export const logIn = (user: UserProps) => ({
+    type: LOG_IN,
+    payload: user,
+});
+
+export const authStateChanged = (user: UserProps) => ({
+    type: AUTH_STATE_CHANGED,
+    payload: user,
+});
