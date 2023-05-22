6a58fd75ce11b0525f0d6e32ff3af81047b08a49
Ibimina hart <ibimina.c.hart@gmail.com>
2023-05-22 08:31:51 +0100

    docs: export github and google auth provider, update project env variable

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,13 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { GithubAuthProvider, getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
+
 
 const firebaseConfig = {
 	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
 	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
 	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
@@ -20,5 +21,6 @@ initializeApp(firebaseConfig);
 const firebaseStore = getFirestore();
 const firebaseAuth = getAuth();
 const firebaseStorage = getStorage();
-
-export { firebaseStore, firebaseAuth, firebaseStorage };
+const googleAuthProvider = new GoogleAuthProvider();
+const githubAuthProvider = new GithubAuthProvider();
+export { firebaseStore, firebaseAuth, firebaseStorage, googleAuthProvider, githubAuthProvider };
