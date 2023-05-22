e96b97b9c090d4a1a2cea0fb529e321076296103
Ibimina hart <ibimina.c.hart@gmail.com>
2023-05-22 08:43:46 +0100

    docs: add google and github login action

--- src/store/action.ts
+++ src/store/action.ts
@@ -4,6 +4,8 @@ export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
 export const LOG_IN = 'LOG_IN';
 export const AUTH_STATE_CHANGED = 'AUTH_STATE_CHANGED';
+export const GOOGLE_AUTH = 'GOOGLE_AUTH';
+export const GITHUB_AUTH = 'GITHUB_AUTH'
 
 export const signIn = (user: UserProps) => ({
 	type: SIGN_IN,
@@ -23,3 +25,13 @@ export const authStateChanged = (user: UserProps) => ({
 	type: AUTH_STATE_CHANGED,
 	payload: user,
 });
+
+export const googleAuth = (user: UserProps) => ({
+	type: GOOGLE_AUTH,
+	payload: user,
+})
+
+export const githubAuth = (user: UserProps) => ({
+	type: GITHUB_AUTH,
+	payload: user,
+})
