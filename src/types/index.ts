3aa2e98cca3685696f43510c1b0b6ad99694f785
Ibimina hart <ibimina.c.hart@gmail.com>
2023-05-30 20:21:35 +0100

    docs: export defaults for components and types

---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export type { ArticleProps } from "./article";
+export type { LoginProps } from "./login";
+export type { SignUpProps } from "./signup";
+export type { UserProps } from "./user";
+export type { CommentProps } from "./article";
+export type { UnSplash } from "./unsplash";
