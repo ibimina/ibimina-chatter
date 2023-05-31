068d6c0551903d08168303af9415362a2fe55476
Ibimina hart <ibimina.c.hart@gmail.com>
2023-05-31 20:11:21 +0100

    docs: refactor user state dispatch update

--- src/hooks/useGithubSigin.ts
+++ src/hooks/useGithubSigin.ts
@@ -1,7 +1,9 @@
-import { firebaseAuth, googleAuthProvider } from "@/firebase/config";
+import { firebaseAuth, firebaseStore, googleAuthProvider } from "@/firebase/config";
 import { githubAuth } from "@/store/action";
 import { useAuthContext } from "@/store/store";
 import { signInWithPopup } from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import router from "next/router";
 
 export const useGitHubSignin = () => {
     const { dispatch } = useAuthContext()
@@ -10,9 +12,17 @@ export const useGitHubSignin = () => {
         try {
             const result = await signInWithPopup(firebaseAuth, googleAuthProvider)
             const user = result.user;
-       
-            dispatch(githubAuth(user))
-            // ...
+            const docRef = doc(firebaseStore, "users", user.uid);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                dispatch(githubAuth(docSnap.data()))
+                router.push('/chatter');
+            } else {
+                const userInfo = { uid: user?.uid, displayName: user?.displayName, email: user?.email, photoURL: user?.photoURL }
+                await setDoc(doc(firebaseStore, "users", user?.uid), userInfo);
+                dispatch(githubAuth(userInfo))
+                router.push('/tags');
+            }
         } catch (error: any) {
             // Handle Errors here.
             const errorCode = error.code;
