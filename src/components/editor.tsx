1dd55103d3cff6d15ae009204cf804e8f2cedf9c
Ibimina hart <ibimina.c.hart@gmail.com>
2023-06-01 16:12:15 +0100

    docs: add auth guard

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -6,8 +6,10 @@ import Buttons from "./markdownbutton";
 import styles from "../styles/editor.module.css";
 import Image from "next/image";
 import { EmojiClickData } from "emoji-picker-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useFetch from "@/hooks/useFetch";
+import { useRouter } from "next/router";
+import { useAuthContext } from "@/store/store";
 
 interface NewType {
     articleDetails: any;
@@ -19,12 +21,16 @@ interface NewType {
     isUnsplashVisible: boolean;
     toggleUnsplash: () => void;
     uploadImage: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    isvisible: boolean;
 }
 
-function Editor({ unsplashSearch, isUnsplashVisible, uploadImage, toggleUnsplash, getUnsplashTerm, articleDetails, handleValueChange, insertMarkdown, getUnSplashUrl }: NewType) {
-    const [isvisible, setIsVisible] = useState<boolean>(false);
+function Editor({ unsplashSearch, isUnsplashVisible, uploadImage, isvisible, toggleUnsplash, getUnsplashTerm, articleDetails, handleValueChange, insertMarkdown, getUnSplashUrl }: NewType) {
+   
     const [showEmojiPicker, setShowEmojiPicker] = useState<boolean>(false);
+    const [hide, setHide] = useState<boolean>(true);
     const [page, setPage] = useState<number>(1);
+    const { state } = useAuthContext();
+    const router = useRouter();
 
     const toggleEmojiPicker = () => {
         return setShowEmojiPicker(!showEmojiPicker)
@@ -48,26 +54,28 @@ function Editor({ unsplashSearch, isUnsplashVisible, uploadImage, toggleUnsplash
         }
     }
 
-    return (<>
-        <button
-            onClick={() => setIsVisible(!isvisible)}
-            className={`${styles.menu}`}
-            aria-label='menu'
-        ></button>
-        <main className={`lg:grid lg:grid-cols-7 lg:gap-2 ${styles.editorGrid}`}>
+
+    useEffect(() => {
+        if (router.pathname.includes("/edit") && state?.user?.uid) {
+            setHide(false)
+        }
+    }, [router.pathname, state?.user?.uid])
+    return (
+    <>
+        <main className={`lg:grid lg:grid-cols-9 gap-6 px-4  ${styles.editorGrid}`}>
             <ArticleSide isvisible={isvisible} />
-            <section className={`col-span-6 ${styles.editSection}`} data-shrink={isvisible}>
+            <section className={`col-span-7 ${styles.editSection}`} data-shrink={isvisible}>
                 {
                     articleDetails?.coverImageUrl ?
                         <div className={`relative w-full h-96 mb-2`}>
-                            <Image src={articleDetails?.coverImageUrl} alt="" fill
+                            <Image src={articleDetails?.coverImageUrl} alt="cover image" fill
                                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw,  33vw" />
                         </div>
                         : <button className={`mb-1`}>Add cover image</button>
                 }
                 <div className={`px-3 md:px-5 lg:px-0`}>
                     <div className={`relative`}>
-                        <div className={`flex items-center mb-1 gap-2`}>
+                        {hide && <div className={`flex items-center mb-1 gap-2`}>
                             <label className={`${styles.fileInput}`}>
                                 <input type="file" onChange={uploadImage} />
                                 <span>upload image</span>
@@ -92,37 +100,58 @@ function Editor({ unsplashSearch, isUnsplashVisible, uploadImage, toggleUnsplash
                                     }
                                 </div>
                             </div>
-                        </div>
-
+                        </div>}
                     </div>
-                    <textarea placeholder='Title' name="title" rows={1}
-                        value={articleDetails.title} className={`block mb-2 w-full p-2`}
-                        onChange={handleValueChange} />
-                    <textarea placeholder='subtitle' name="subtitle" rows={1}
-                        value={articleDetails.subtitle} className={`block mb-2 w-full p-2`}
-                        onChange={handleValueChange}
-                    />
-                    <Buttons
+                    {
+                        hide ?
+                            <textarea placeholder='Title' name="title" rows={1}
+                                value={articleDetails.title} className={`block mb-2 w-full p-2`}
+                                onChange={handleValueChange} /> :
+                            <h1 className={`font-bold text-3xl mb-4`}>{articleDetails.title}</h1>
+
+                    }
+                    {
+                        hide ? 
+                            <textarea placeholder='subtitle' name="subtitle" rows={1}
+                                value={articleDetails.subtitle} className={`block mb-2 w-full p-2`}
+                                onChange={handleValueChange}
+                            />
+                            : <h2 className={`font-medium text-2xl mb-4`}>{articleDetails.subtitle}</h2>
+                    }
+
+                    {hide && <Buttons
                         handleClick={insertMarkdown}
                         toggleEmojiPicker={toggleEmojiPicker}
                         showEmojiPicker={showEmojiPicker}
                         selectEmoji={handleEmojiSelect}
-                    />
-                    <div className={`grid grid-cols-2 gap-2 ${styles.markdownWrapper}`}>
-                        <div className={`${styles.markdown}`}>
-                            <h1>Markdown Editor</h1>
-                            <textarea
-                                name="article"
-                                id="markdownTextarea"
-                                rows={10}
-                                value={articleDetails.article}
-                                onChange={(e) => handleValueChange(e)}
-                                placeholder="Enter Markdown text"
-                                className={`${styles.textarea} w-full p-3 border-2 border-solid border-current`}
-                            />
-                        </div>
-                        <div>
-                            <h2>Preview</h2>
+                    />}
+
+                    <div className={`flex w-full gap-2 ${styles.markdownWrapper}`}>
+                        {
+                            hide && <div className={`${styles.markdown} w-full`}>
+                                <h1 className={`font-medium  max-w-max p-2 rounded-lg`}>Editor</h1>
+                                <textarea
+                                    name="article"
+                                    id="markdownTextarea"
+                                    rows={10}
+                                    value={articleDetails.article}
+                                    onChange={(e) => handleValueChange(e)}
+                                    placeholder="Enter Markdown text"
+                                    className={` ${styles.textarea} w-full p-3 border-2 border-solid border-current`}
+                                />
+                            </div>
+
+                        }
+                        <div className={`w-full `}>
+                            <div className={`flex items-center gap-2 mb-2`}>
+                                {hide && <h2 className={` font-medium  max-w-max p-2 rounded-lg`}>Preview</h2>}
+                                    <button
+                                        onClick={() => setHide(!hide)}
+                                        className={`border-2 border-violet-400 font-medium
+                                          max-w-max py-1 px-6 rounded-3xl`}> 
+                                          {hide && router ? "close editor" :"edit"}</button>
+                            </div>
+
                             <ReactMarkdown remarkPlugins={[remarkGfm]}
                                 components={{ a: LinkRenderer }}
                                 className={` prose prose-headings:m-0 prose-p:m-0.6 
