17bef8172ee2fd24be729097bda5793e92e70029
Ibimina hart <ibimina.c.hart@gmail.com>
2023-06-01 16:13:18 +0100

    docs: refactor code

--- src/components/editorheader.tsx
+++ src/components/editorheader.tsx
@@ -1,18 +1,21 @@
 import Image from 'next/image';
 import { useAuthContext } from '@/store/store';
 import { useRouter } from 'next/router';
-
+import styles from "../styles/editor.module.css";
 interface NewType {
     publishArticleInFirebase: (e: React.MouseEvent) => Promise<void>;
     changeRoute?: (route: string) => void;
+    isvisible: boolean;
+    handleVisible: (e: React.MouseEvent) => void;
 }
 
-function EditorHeader({ publishArticleInFirebase, changeRoute }: NewType) {
+function EditorHeader({ publishArticleInFirebase, changeRoute,handleVisible }: NewType) {
+ 
     const { state } = useAuthContext();
     const route = useRouter()
     return (
         <header
-            className={`grid grid-cols-3 lg:grid-cols-7 items-center py-3 px-6`}
+            className={`grid grid-cols-3 lg:grid-cols-9 items-center py-3 px-6`}
         >
             <div className={`flex items-center gap-2 col-span-1 md:col-span-1`}>
                 {/* <button
@@ -23,16 +26,20 @@ function EditorHeader({ publishArticleInFirebase, changeRoute }: NewType) {
                 {
                     route.pathname === "/post"
                         ?
-                        <h1 className={`text-3xl font-bold underline text-red-300`}><button onClick={() => changeRoute!("chatter")}> Chatter</button></h1>
+                        <h1 className={`text-3xl font-bold underline`}><button onClick={() => changeRoute!("chatter")} className='underline'> Chatter</button></h1>
                         : <h1 className={`text-3xl font-bold underline`}> Chatter</h1>
                 }
 
             </div>
-
-            <nav className={`col-start-3 lg:col-start-6 lg:col-end-8`}>
+            <button
+             onClick={handleVisible}
+                className={`${styles.menu} col-start-3`}
+                aria-label='menu'
+            ></button>
+            <nav className={`col-start-3 lg:col-start-7 lg:col-end-10`}>
                 <ul className={`flex items-center gap-2 justify-end`}>
                     <li
-                        className={`relative p-1 rounded-3xl lg:py-2 lg:px-6  border-2 border-current`}>
+                        className={`relative p-1 rounded-3xl lg:py-2 lg:px-6 bg-violet-700 font-medium text-slate-50 focus:border-2 focus:border-current`}>
                         <button onClick={publishArticleInFirebase} className={`block`}>
                             {
                                 route.pathname.includes("/edit") ? "Update" : "Publish"
@@ -56,7 +63,6 @@ function EditorHeader({ publishArticleInFirebase, changeRoute }: NewType) {
                             alt='user'
                             className={`rounded-full`}
                         />
-
                     </li>
                 </ul>
             </nav>
