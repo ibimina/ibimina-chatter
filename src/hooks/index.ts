37b3943b5c0b44454fa317ba87e2093713b3bbfd
Ibimina hart <ibimina.c.hart@gmail.com>
2023-06-09 14:45:29 +0100

    chore: add export default for hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,10 @@
+export { default as useCollection } from './useCollection';
+export {default as useCollectionSnap} from './useCollectionSnap';
+export {default as useEditor} from './useEditor';
+export {default as useFetch} from './useFetch';
+export {default as useLogin} from './useLogin';
+export {default as useLogout} from './useLogout';
+export {default as useSignUp} from './useSignUp';
+export {default as useGithubSigin} from './useGithubSigin';
+export {default as useGoogleSignin} from './useGoogleSignin';
+export {default as useInteraction} from "./useInteraction"
