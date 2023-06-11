707bf4ebeacdb8ac53f650a108e332f8ddae26a7
Ibimina hart <ibimina.c.hart@gmail.com>
2023-06-11 22:30:46 +0100

    docs: testing

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,15 +2,16 @@ import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export default function middleware(req: NextRequest) {
+    // req.locals is a Map
     let verify = req.cookies.get("loggedin");
     let url = req.url
 
     if (!verify && url.includes('/settings') || url.includes('/chatter') || url.includes('/tags') || url.includes('/bookmarks') || url.includes('/explore') || url.includes('/post')) {
-        return NextResponse.redirect("/");
+        return NextResponse.redirect("https://ibimina-chatter-git-feat-single-article-ibimina.vercel.app/");
     }
 
-    if (verify && url === "/" || url.includes("/signup" || url.includes("/forgotpassword"))) {
-        return NextResponse.redirect("/chatter");
+    if (verify && url === "https://ibimina-chatter-git-feat-single-article-ibimina.vercel.app/") {
+        return NextResponse.redirect("https://ibimina-chatter-git-feat-single-article-ibimina.vercel.app/chatter");
     }
     }
    
