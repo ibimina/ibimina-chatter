fc9e15be1ced7bfde446ee9ef88b37020eb2c97f
Ibimina hart <ibimina.c.hart@gmail.com>
2023-06-14 13:25:56 +0100

    refactor: change tags to topics

--- src/types/user.ts
+++ src/types/user.ts
@@ -3,7 +3,7 @@ export interface UserProps {
 	email: string | null;
 	displayName: string | null;
 	photoURL: string | null;
-	tags: string[] ;
+	topics: string[] ;
 	profile_tagline: string;
 	location: string;
 	bio: string;
