7961e125e6f6b11ea528520795e5b2c1babc8134
Ibimina hart <ibimina.c.hart@gmail.com>
2023-06-24 02:38:59 +0100

    refactor: unspalsh api fetch logic

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -3,20 +3,28 @@ import { useEffect, useState } from "react";
 
 
 
-function useFetch(url: string) {
-
+function useFetch(unsplashSearch: string, page: number) {
   const [fetchedData, setFetchedData] = useState<UnSplash[]>([]);
-  const getData = async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    setFetchedData([...fetchedData,...data.results]);
-  }
+  const url = 'https://api.unsplash.com/search/photos?query='
   useEffect(() => {
-    getData()
-    return () => { getData()}
-  })
+    const getData = async () => {
+      const response = await fetch(`${url}${unsplashSearch}&client_id=${process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}&page=${page}`);
+      const data = await response.json();
+      if(page === 1){
+        setFetchedData([ ...data.results]);
+      }else{
+        setFetchedData((fetchedData) => [...fetchedData, ...data.results]);
+      }    
+    }
+    getData();
+  },[unsplashSearch, page])
 
-  return { fetchedData, getData };
+  const getSearchData = async () => {
+    const response = await fetch(`${url}${unsplashSearch}&client_id=${process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}&page=${page}`);
+    const data = await response.json();
+    setFetchedData([ ...data.results]);
+  }
+  return { fetchedData,getSearchData };
 }
 
 export default useFetch;
