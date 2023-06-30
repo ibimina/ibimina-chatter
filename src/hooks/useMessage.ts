4c62ac2b2147aa3c1c81caac2d6afe9c53fdecf6
Ibimina hart <ibimina.c.hart@gmail.com>
2023-06-30 15:50:43 +0100

    docs: refactor messages hook

--- src/hooks/useMessage.ts
+++ src/hooks/useMessage.ts
@@ -1,26 +1,29 @@
 import { firebaseStore } from "@/firebase/config";
 import { useAuthContext } from "@/store/store";
-import { onSnapshot, doc, DocumentData } from "firebase/firestore";
-import { useEffect, useState } from "react";
+import { onSnapshot, DocumentData, collection,  } from "firebase/firestore";
+import {  useEffect, useState } from "react";
 
 function useMessage() {
-    const [messages,setMessages] = useState<DocumentData>()
-    const {state} = useAuthContext()
+    const [messages, setMessages] = useState<DocumentData>()
+    const { state } = useAuthContext()
 
     useEffect(() => {
-        if ( state?.user?.uid){
-            onSnapshot(doc(firebaseStore, "messages", `${state?.user?.uid}`), (doc) => {
-                if (doc.exists() && doc.data()?.following?.length > 0) {
-                    setMessages(doc.data()?.following)
+        if (state?.user?.uid.length > 0) {
+            const getMessages = async () => {
+                const q = collection(firebaseStore, "messages", `${state?.user?.uid}`, "chats")
+                onSnapshot(q, (snap) => {
+                    let arr: DocumentData  = []
+                    snap.forEach((d) => {
 
-                } else {
-                    setMessages([])
-                }
-            });
+                        arr.push(d.data()?.contact)
+                    })
+                    setMessages(arr)
+                })
+            }
+            getMessages()
         }
-        // return () => unsub() 
     }, [state?.user?.uid])
-    return {messages}
+    return { messages }
 }
 
 export default useMessage;
