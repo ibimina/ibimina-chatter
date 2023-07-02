69cb7c6d73cb4680c469abd3cc446c5d9779b511
Ibimina hart <ibimina.c.hart@gmail.com>
2023-07-02 13:34:37 +0100

    docs: change link, add comment image

--- src/components/notificationcard.tsx
+++ src/components/notificationcard.tsx
@@ -18,10 +18,14 @@ function NotificationCard({ notification }: {
         <>
             <div className="py-4" >
                 <div className="">
-                    <Link href={`/${encodeURIComponent(author)}`} className="flex items-center gap-2 mb-2">
+                    <Link href={`/profile/${encodeURIComponent(author)}`} className="flex items-center gap-2 mb-2">
                         {
                             notification.event === "liked" &&
                             <Image src='/images/icons8-love-48.png' height={24} width={24} alt="like" />}
+                            {
+                            notification.event === "commented" &&
+                            <Image src='/images/icons8-comment-24.png' height={24} width={24} alt="comment" />
+                            }
                         {
                             notification.event_userimage === null &&
                             <Image className={`rounded-full`} src="/images/icons8-user.svg" width={30} height={30} alt={`${notification.event_username}`} />
@@ -35,7 +39,7 @@ function NotificationCard({ notification }: {
                     <p className="text-sm">
                         <span className="text-gray-900 leading-none font-bold">@{notification?.event_username}  </span>
                         <span className="text-gray-600">{notification?.event} {notification?.event === "liked"? "your article" : "on your article"}  </span>
-                        <Link className="text-gray-600 font-medium" href={`/${encodeURIComponent(author)}/${encodeURIComponent(id)}`}>{notification?.articleTitle}</Link>
+                        <Link className="text-gray-600 font-medium" href={`/inkspire/${encodeURIComponent(id)}`}>{notification?.articleTitle}</Link>
                     </p>
                 </div>
             </div>
