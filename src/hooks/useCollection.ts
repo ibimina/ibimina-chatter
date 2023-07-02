a7677fa29568abc7bd9b0129fade6a51b53c1907
Ibimina hart <ibimina.c.hart@gmail.com>
2023-07-02 13:41:04 +0100

    docs: change getdoc with onsnapshot

--- src/hooks/useCollection.ts
+++ src/hooks/useCollection.ts
@@ -1,23 +1,27 @@
 import { firebaseStore } from "@/firebase/config";
-import { DocumentData, doc, getDoc,  } from "firebase/firestore";
+import { DocumentData, doc, onSnapshot, } from "firebase/firestore";
 import { useEffect, useState } from "react"
 
-const useCollection = (collRef:string,user: string,) => {
+const useCollection = (collRef: string, user: string,) => {
     const [data, setData] = useState<DocumentData>([])
 
     useEffect(() => {
         const getData = async () => {
             const docRef = doc(firebaseStore, collRef, user);
-            const docSnap = await getDoc(docRef);
-
-            if (docSnap.exists()) {         
-                setData({...docSnap.data(),id:docSnap.id})
-            } 
+            onSnapshot(docRef, (doc) => {
+                console.log(doc.data())
+                if (doc.exists()) {
+                    setData({ ...doc.data(), id: doc.id })
+                } else {
+                    setData([])
+                }
+            })
         }
         if (user?.length > 1) {
             getData()
         }
-    }, [collRef, user, user?.length])
+
+    }, [collRef, user])
 
     return { data }
 }
